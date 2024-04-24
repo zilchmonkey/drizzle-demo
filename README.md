3798d869b2bfd1e7eb9feb7dba504af24a31e807
Andre Okamura <zilchmonkey@hotmail.com>
2024-04-23 20:46:49 -0400

    Adding initial files.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# drizzle-demo
-Drizzle demo. Curious how this ORM works.
+# Drizzle
+
+[Drizzle](https://orm.drizzle.team/) is an extremely flexible TypeScript ORM. Supports many different databases including PostgreSQL, MySQL, SQLite and many more.
+
+# Directions
+
+- run `npm i` in your terminal.
+- Create an .env file and define a database url. This example uses postgreSQL. For example:
+  `DATABASE_URL="postgres://user:password@localhost:port/database_name"`
+- You will also need to manually create a database. We will be migrating the tables in. Database name should be the same as defined in your .env file.
+- In your terminal run `npm run db:generate`. This will create the migration files.
+- In your terminal run `npm run db:migrate`. If your connection to the database is correct and the database exists this should create the tables and columns within it.
+
+Done! You should be good to go. Right now the database should be empty. You'll need to insert data. In the `/src/main.ts` file try adding the following to the main function:
+
+```javascript
+const user = await db
+  .insert(UserTable)
+  .values([
+    {
+      name: "Sean",
+      age: 29,
+      email: "test@test.com",
+    },
+    { name: "Angus", age: 34, email: "anothertest@test.com" },
+  ])
+  .returning({
+    id: UserTable.id,
+    userName: UserTable.name,
+  })
+user.map((u) => {
+  console.log(`${u.userName} has been added!`)
+})
+```
+
+The `/src/main.ts` file also has more query examples commented out. Feel free to uncomment them and try it out!
---
+++ src/main.ts
@@ -0,0 +1,56 @@
+// import { sql } from 'drizzle-orm'
+import { asc, count, desc, eq, gt } from "drizzle-orm"
+import { db } from "./drizzle/db"
+import { UserPreferencesTable, UserTable } from "./drizzle/schema"
+
+async function main() {
+  // await db.delete(UserTable)
+  // const user = await db
+  //   .insert(UserTable)
+  //   .values([
+  //     {
+  //       name: "Sean",
+  //       age: 29,
+  //       email: "test@test.com",
+  //     },
+  //     { name: "Angus", age: 34, email: "anothertest@test.com" },
+  //   ])
+  //   .returning({
+  //     id: UserTable.id,
+  //     userName: UserTable.name,
+  //   })
+  // user.map((u) => {
+  //   console.log(`${u.userName} has been added!`)
+  // })
+
+  // await db.insert(UserPreferencesTable).values({
+  //     emailUpdates: true,
+  //     userId: "b927b80a-34ef-442b-8f85-ed178d020057"
+  // })
+
+  // const users = await db.query.UserTable.findMany({
+  //     columns: { email: true, id: true, age: true },
+  //     // extras: { lowerCaseName: sql<string>`lower(${UserTable.name})`.as("lowerCaseName") }
+  //     // limit: 1
+  //     // offset: 1
+  //     // with: {
+  //     //     preferences: {
+  //     //         columns: {
+  //     //             emailUpdates: true
+  //     //         }
+  //     //     },
+  //     //     posts: { with: { postCategories: true } }
+  //     // }
+  //     // orderBy: desc(UserTable.age)
+  //     orderBy: (table, funcs) => funcs.desc(table.age)
+  // })
+
+  const users = await db
+    .select({ age: UserTable.age, count: count(UserTable.name) })
+    .from(UserTable)
+    .groupBy(UserTable.age)
+
+  console.log(users)
+}
+
+main()
